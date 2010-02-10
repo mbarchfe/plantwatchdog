a5af9afe2e900e931dda2bd0b53fab92695f3f6e
Markus Barchfeld <mbarchfe@googlemail.com>
2010-02-10 14:24:51 +0100

    add Array.prototype.filter for IE

--- public/js/customflot.js
+++ public/js/customflot.js
@@ -1,3 +1,28 @@
+// IE
+if (!Array.prototype.filter)
+{
+  Array.prototype.filter = function(fun /*, thisp*/)
+  {
+    var len = this.length >>> 0;
+    if (typeof fun != "function")
+      throw new TypeError();
+
+    var res = [];
+    var thisp = arguments[1];
+    for (var i = 0; i < len; i++)
+    {
+      if (i in this)
+      {
+        var val = this[i]; // in case fun mutates this
+        if (fun.call(thisp, val, i, this))
+          res.push(val);
+      }
+    }
+
+    return res;
+  };
+}
+
 function Graph(placeholder_id) {
     this.placeholder_id = placeholder_id
     this.placeholder = $(placeholder_id);
